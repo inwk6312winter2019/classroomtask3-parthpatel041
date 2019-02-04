444867d868e5808707c7220a26c4611b73bec9cf
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-04 07:13:06 -0800

    task performed

---
+++ card.py
@@ -0,0 +1,18 @@
+class card():
+
+  suit_names = ['clubs','diamonds','hearts','spades']
+  rank_names = [none,'ace','2','3','4','5','6','7','8','9','10','jack','$
+
+def __init__(self,rank=0,suit=2):
+
+  self.rank = rank
+  self.suit = suit
+
+def __str__(self):
+   return f"the rank is {rank_names[suit.suit]} and the suit is {suit_na$
+
+ace_of_spade = card(1,3)
+print(ace_of_spade)
+
+
+
